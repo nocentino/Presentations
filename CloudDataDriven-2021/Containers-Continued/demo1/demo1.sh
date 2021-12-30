1b4e1cacc995af955c8c31de6d5427e8b4ef07d8
Anthony E. Nocentino <aen@centinosystems.com>
2021-12-30 12:10:21 -0600

    CloudDataDriven

---
+++ CloudDataDriven-2021/Containers-Continued/demo1/demo1.sh
@@ -0,0 +1,49 @@
+#log into our linux host
+ssh aen@docker0
+cd ../demo1
+
+
+#Let's build that image
+docker build -t sqlimagedemo1 . 
+
+
+#Run a container
+docker run \
+    --env 'ACCEPT_EULA=Y' \
+    --env 'MSSQL_SA_PASSWORD=S0methingS@Str0ng!' \
+    --name 'sqldemo1' \
+    --hostname 'sqldemo1' \
+    --volume sqldata1:/var/opt/mssql \
+    --publish 31433:1433 \
+    --detach sqlimagedemo1
+
+
+#Is the container running?
+docker ps
+
+
+#Can I connect to SQL Server?
+sqlcmd -S localhost,31433 -U sa -Q 'SELECT @@SERVERNAME' -P 'S0methingS@Str0ng!'
+sqlcmd -S localhost,31433 -U sa -Q 'SELECT @@VERSION' -P 'S0methingS@Str0ng!'
+
+
+#Which user is running mssql? And let's check out the mssql-conf file
+docker exec -it sqldemo1 /bin/bash
+ps -aux --forest
+cat /var/opt/mssql/mssql.conf
+env
+ls -la /var/opt/mssql
+ls -la /var/opt/mssql/data
+exit
+
+
+#Where is the volume really?
+docker volume inspect sqldata1
+sudo ls -la /var/lib/docker/volumes/sqldata1/_data
+sudo ls -la /var/lib/docker/volumes/sqldata1/_data/data
+
+
+#Stop and delete that container
+docker rm -f sqldemo1
+docker volume rm sqldata1
+sudo ls -la /var/lib/docker/volumes
---
+++ CloudDataDriven-2021/Containers-Continued/demo2/demo2.sh
@@ -0,0 +1,75 @@
+cd ../demo2
+
+
+#Demo 1 - Mount and external volume into the container and restore backups
+#You can COPY the database into the container during the build, but then this can cause your container to grow
+#This will keep the weight of the databases outside of the container.
+docker run \
+    --env 'ACCEPT_EULA=Y' \
+    --env 'MSSQL_SA_PASSWORD=S0methingS@Str0ng!' \
+    --name 'sqldemo2a' \
+    --hostname 'sqldemo2a' \
+    --publish 31433:1433 \
+    --volume sqldata2a:/var/opt/mssql \
+    --volume ~/content/Containers-WhatsNext/backups:/backup \
+    --detach sqlimagedemo1
+
+
+#You can copy backups into the container...but why???  Time...space???
+#docker cp ../backups/TestDB1.bak sqldemo2a:/backup
+#docker exec -u root sqldemo2a chown mssql /var/opt/mssql/data/TestDB1.bak
+#sqlcmd -S localhost,31433 -U sa -i restore_testdb1.sql -P 'S0methingS@Str0ng!'
+
+
+#Check out the contents of the mounted backup folder
+ls -lah ~/content/Containers-WhatsNext/backups
+docker exec -it sqldemo2a ls -lah /backup
+sqlcmd -S localhost,31433 -U sa -i ./scripts/restore_testdb1.sql -P 'S0methingS@Str0ng!'
+
+
+#Get a list of databases
+sqlcmd -S localhost,31433 -U sa -Q 'SELECT name from sys.databases' -P 'S0methingS@Str0ng!' -W
+
+
+#Clean up from demo
+docker rm -f sqldemo2a
+docker volume rm sqldata2a
+
+
+
+#Demo 2 - Mount and external volume into the container and restore backups on container startup
+#Build a new image pointing CMD to /scripts/configure-db.sh 
+docker build -t sqlimagedemo2 . 
+
+
+#Run a container that will restore a database automatically
+docker run \
+    --env 'ACCEPT_EULA=Y' \
+    --env 'MSSQL_SA_PASSWORD=S0methingS@Str0ng!' \
+    --name 'sqldemo2b' \
+    --hostname 'sqldemo2b' \
+    --publish 31434:1433 \
+    --volume sqldata2b:/var/opt/mssql \
+    --volume ~/content/Containers-WhatsNext/backups:/backup \
+    --detach sqlimagedemo2
+
+
+#Attach to the log to watch the start up process...one the engine is online, the restore will start
+docker logs sqldemo2b --follow
+
+
+#Get a list of databases
+sqlcmd -S localhost,31434 -U sa -Q 'SELECT name from sys.databases' -P 'S0methingS@Str0ng!' -W
+
+
+#Let's look at where those files and backups are inside the container
+docker exec -it sqldemo2b /bin/bash
+ls /backup
+ls /scripts
+ps -aux --forest
+exit
+
+
+#Clean up from demo
+docker rm -f sqldemo2b 
+docker volume rm sqldata2b
